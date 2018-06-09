d7fd648e78cb04fe40a1c02f34bef7855dbbf669
Erick Henrique dos Santos <erickhenrique321@gmail.com>
2018-06-09 19:03:04 -0300

    mudanças fontes e imagem principal de download

--- js/main.js
+++ js/main.js
@@ -1,37 +1,27 @@
 
-var documentEl = $(document),
-    fadeElem = $('.fadein');
-$('.fadein').style = 'opacity:0';
+var documentEl = $(document);
+//     fadeElem = $('.fade');
+// $('.fade').style = 'opacity:0';
 
 
 documentEl.on('scroll', function () {
+    // $('#navbarText').on('activate.bs.scrollspy', function () {
+    //     alert("ativou");
+    //    });
     if (window.pageYOffset > 100) {
         document.getElementById('barra-navegacao').style.backgroundColor = '#333e50';
-      
-      
+
+
     }
-     else {
+    else {
         document.getElementById('barra-navegacao').style.backgroundColor = 'transparent';
     }
-    var currScrollPos = documentEl.scrollTop();
-
-    fadeElem.each(function () {
-        var $this = $(this),
-            elemOffsetTop = $this.offset().top;
-        if (currScrollPos + 400 > elemOffsetTop){
-          
-            $this.addClass('animated fadeInRight');
-            $this.css('opacity', 1 - (currScrollPos - elemOffsetTop) / 500);
-        }
-         
-        
-       
-    });
+  
 });
 
 
 $('#carouselComentarios').carousel({
-    interval: 2100
+    interval: 2200
 });
 
 
